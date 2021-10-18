fe022d242552c7511453add3555c73a1d701461c
Danielle_Leslie <danielleleslie@mail.com>
2021-10-18 00:40:53 -0500

    Activity Upload Completion

--- activity_3/API/Startup.cs
+++ activity_3/API/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using API;
 
-
 namespace API
 {
     public class Startup
@@ -24,23 +23,11 @@ namespace API
             Configuration = configuration;
         }
 
-        private readonly string CorsPolicy = "CorsPolicy";
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy(CorsPolicy, policyBuilder =>
-                {
-                    policyBuilder
-                    .WithOrigins("http://localhost:3000")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-                });
-            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(opt =>
             {
@@ -62,7 +49,6 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-            app.UseCors(CorsPolicy);
             app.UseMvc();
         }
     }
